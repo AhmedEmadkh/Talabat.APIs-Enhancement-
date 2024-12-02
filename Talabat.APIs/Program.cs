6f2a4e07950b97fb42d20d4d94d8d51b1a635145
AhmedEmadkh <ae3866948@gmail.com>
2024-12-02 14:15:43 +0200

    Not Found EndPoint Handling

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -83,7 +83,7 @@ namespace Talabat.APIs
 				app.UseSwagger();
 				app.UseSwaggerUI();
 			}
-
+			app.UseStatusCodePagesWithReExecute("errors/{0}");
 			app.UseHttpsRedirection();
 
 			app.UseStaticFiles();
