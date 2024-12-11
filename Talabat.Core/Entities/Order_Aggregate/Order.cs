ddfff46af1fb9af2d7bfcba53380bbf1479199d0
AhmedEmadkh <ae3866948@gmail.com>
2024-12-11 17:13:54 +0200

    OrderItem Module Entities

---
+++ Talabat.Core/Entities/Order_Aggregate/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities.Order_Aggregate
+{
+    public class Order : BaseEntity
+    {
+        public string BuyerEmail { get; set; }
+        public DateTimeOffset OrderDateDate { get; set; } = DateTimeOffset.UtcNow;
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public Address ShippingAddress { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; } // Navigational Property [ONE]
+        public ICollection<OrderItem> Items { get; set; } = new HashSet<OrderItem>();
+        public decimal Subtotal { get; set; }
+        public string PaymentIntentId { get; set; } = string.Empty;
+
+        public decimal GetTotal() 
+            => Subtotal + DeliveryMethod.Cost;
+    }
+}
